37f9abc8f846fa8d27d22f9a6befa51cc903300e
stackTom <rtiky23@gmail.com>
2016-01-22 22:44:31 -0500

    fixed incorrect detection of touches on sprites; began adding support for pan gesture to select sprites

--- Block Game/GameScene.swift
+++ Block Game/GameScene.swift
@@ -8,18 +8,19 @@
 
 import SpriteKit
 
+// TODO: need a way to determine if a sprite was touched or not. we could use a dictionary with key = name of tile, and value is the tile object itself.
 class GameScene: SKScene {
     static let NUM_ROWS = 3
     static let NUM_COLUMNS = 3
     
     var tiles: [[Tile]] = [[Tile]]()
-
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
         self.size = view.bounds.size
         
         // set up tiles Array
-        print("\(self.size.height)")
+        
         for var column = 0; column < GameScene.NUM_COLUMNS; column++ {
             let tileRow = [Tile]()
             tiles.append(tileRow)
@@ -32,27 +33,43 @@ class GameScene: SKScene {
                 let tileSprite = tiles[column][row].sprite!
                 
                 // name = row, column to identify sprite at specific location
-                tileSprite.name = String(row) + "," + String(column)
+                tileSprite.name = String(row) + ", " + String(column)
                 
-                tileSprite.size = CGSizeMake(CGFloat(50.0), CGFloat(50.0))
+                //tileSprite.size = CGSizeMake(CGFloat(50.0), CGFloat(50.0))
                 tileSprite.position = position
                 // add sprite
                 self.addChild(tileSprite)
                 print("added \(column) \(row) at \(position)")
             }
         }
+        
+        // pan recognizer for selecting tiles with a pan
+        let gestureRecognizer = UIPanGestureRecognizer(target: self, action: "didPanOnTiles:")
+        self.view?.addGestureRecognizer(gestureRecognizer)
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-       /* Called when a touch begins */
+        /* Called when a touch begins */
         for touch in touches {
             let position = touch.locationInView(self.view)
-            let touchedSprite = self.nodeAtPoint(position)
+            
+            // convert to the coord system of this scene class
+            let convertedPosition = self.convertPointFromView(position)
+            let touchedSprite = self.nodeAtPoint(convertedPosition)
+            
             
             print(touchedSprite.name)
         }
     }
-   
+    
+    func didPanOnTiles(gestureRecognizer: UIPanGestureRecognizer) {
+        let position = gestureRecognizer.locationInView(self.view)
+        let convertedPosition = self.convertPointFromView(position)
+        let touchedSprite = self.nodeAtPoint(convertedPosition)
+        
+        print(touchedSprite.name)
+    }
+    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
     }
